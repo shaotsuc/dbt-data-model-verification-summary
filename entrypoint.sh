102ffb8c54840a522dc67fb3ee175710726f6b49
Shao-Tsu <shaotsu.chen@gmail.com>
2024-11-30 20:44:40 +0200

    Updated configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ build-db)
     psql -h postgres -d verification -U admin -c "\copy crm.accounts FROM '../data/accounts.csv' DELIMITER ',' CSV"
     psql -h postgres -d verification -U admin -c "\copy prod.clients FROM '../data/clients.csv' DELIMITER ',' CSV"
     psql -h postgres -d verification -U admin -c "\copy prod.verification_sessions FROM '../data/verification_sessions.csv' DELIMITER ',' CSV"
-    psql -h postgres -d verification -U admin -c "\copy prod.documents FROM '../data/documents.csv' DELIMITE ',' CSV"
+    psql -h postgres -d verification -U admin -c "\copy prod.documents FROM '../data/documents.csv' DELIMITER ',' CSV"
       fi
   ;;
 *)
